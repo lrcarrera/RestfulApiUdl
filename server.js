f2a7dcc46e48339af1dc7380f98aea9fc29ccb2d
lrcarrera <drewico@gmail.com>
2019-02-04 17:47:04 +0100

    testing route with client deployed in Heroku

--- server.js
+++ server.js
@@ -25,6 +25,27 @@ mongoose.connect(uristring, function (err, res) {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '34.236.18.196:443');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
 var routes = require('./api/routes/todoListRoutes'); //importing route
 routes(app); //register the route
 
