6fe73c6bfda4e2f232cbca4b1d8c3b1ada815c32
lrcarrera <drewico@gmail.com>
2019-02-07 22:12:24 +0100

    test credentials

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var theport = process.env.PORT || 5000;
 var uristring =
     process.env.MONGOLAB_URI ||
     process.env.MONGOHQ_URL ||
-    'mongodb://heroku_l7t0z2hl:rpihc5nokv98besvm18vh82sdh@ds211625.mlab.com:11625/heroku_l7t0z2hl';
+    'mongodb://drewico:cYJkgSTDgiWVcA6cwdxQX2mrUHuWPbws@ds211625.mlab.com:11625/heroku_l7t0z2hl';
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost/Tododb');
