ffe3e03b1de6e3fa5fc3c716d4be38e0efb90418
lrcarrera <drewico@gmail.com>
2019-02-21 22:06:14 +0100

    fix in profile route

--- api/controllers/profile.js
+++ api/controllers/profile.js
@@ -12,9 +12,24 @@ module.exports.profileRead = function(req, res) {
   //req.get(headerName)
   //res.status(200).json(req.payload._id);
   var token = req.headers.authorization.split(" ")[1];
-  var payload = jwt.decode(token, "MY_SECRET");
+//  var payload = jwt.decode(token, "MY_SECRET");
 
-  if (!payload._id) {
+  jwt.verify(token, 'MY_SECRET', (err, authorizedData) => {
+            if(err){
+                //If error send Forbidden (403)
+                console.log('ERROR: Could not connect to the protected route');
+                res.sendStatus(403);
+            } else {
+                //If token is successfully verified, we can send the autorized data
+                res.json({
+                    message: 'Successful log in',
+                    authorizedData
+                });
+                console.log('SUCCESS: Connected to protected route');
+            }
+        });
+
+/*  if (!payload._id) {
     res.status(401).json({
       "message" : "UnauthorizedError: private profile"
     });
@@ -25,6 +40,6 @@ module.exports.profileRead = function(req, res) {
       .exec(function(err, user) {
         res.status(200).json(user);
       });
-  }
+  }*/
 
 };
