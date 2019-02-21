7f7990b43c27caab046269e198e67344cae95ce2
lrcarrera <drewico@gmail.com>
2019-02-21 21:09:21 +0100

    fix route and add route by default

--- api/controllers/profile.js
+++ api/controllers/profile.js
@@ -6,18 +6,18 @@ module.exports.profileRead = function(req, res) {
   //Do error trapping
   // If no user ID exists in the JWT return a 401
   //req.get(headerName)
-  if (!req.headers.Authorization) {
+  res.status(200).json(req.payload._id);
+  /*if (!req.payload._id) {
     res.status(401).json({
       "message" : "UnauthorizedError: private profile"
     });
   } else {
-    // Otherwise continue
     console.log("Authorized user");
     User
-      .findById(req.headers.Authorization)
+      .findById(req.payload._id)
       .exec(function(err, user) {
         res.status(200).json(user);
       });
-  }
+  }*/
 
 };
