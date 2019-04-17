fcb7bbcf9d093f479012bbe2dfd4600722c22cba
lrcarrera <drewico@gmail.com>
2019-04-17 19:18:22 +0200

    Add trace to debug route

--- api/controllers/customerController.js
+++ api/controllers/customerController.js
@@ -53,6 +53,7 @@ exports.delete_customer = function(req, res) {
 };
 
 exports.insert_new_account = function(req, res) {
+  console.log("log: " + req.body);
   Customer.findOneAndUpdate({ dni : req.params.customerId }, {$push: { accounts : req.body } }, {safe: true, upsert: true}, (err, customer) => {
     if (err)
       res.send(err);
