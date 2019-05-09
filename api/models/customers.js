ceff4f098945a34a265c7f967c5133e007542e9c
lrcarrera <drewico@gmail.com>
2019-05-09 16:15:48 +0200

    Added investment products

--- api/models/customers.js
+++ api/models/customers.js
@@ -1,125 +1,114 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-const ObjectId = Schema.Types.ObjectId;
-//todoListModel
-/*var CustomerSchema = new Schema({
-name: {
-type: String,
-required: 'Kindly enter the name of the customer'
-},
-Created_date: {
-type: Date,
-default: Date.now
-},
-status: {
-type: [{
-type: String,
-enum: ['pending', 'ongoing', 'completed']
-}],
-default: ['pending']
-},
-risk_money_laundering: {
-type: [{
-type: String,
-enum: ['low', 'medium', 'high']
-}],
-default: ['medium']
-},
-email: {
-type: String,
-default: 'no'
-},
-number: {
-type: String,
-required: 'Kindly enter the phone number of customer'
-}
-});
-*/
+
 var CustomerSchema = new Schema({
 
-  dni: {
-    type: String,
-    unique: true,
-    required: 'Kindly enter dni please'
-  },
-  customer_info: {
-    first_name: {
-      type: String
-    },
-    last_name: {
-      type: String
-    },
-    current_address: {
-      type: String,
-    },
-    email_address: {
-      type: String,
-    },
-    risk_money_laundering: {
-      type: [{
+    dni: {
         type: String,
-        enum: ['low', 'medium', 'high']
-      }],
-      default: ['medium']
+        unique: true,
+        required: 'Kindly enter dni please'
     },
-    last_modification_date: {
-      type: Date,
-      default: Date.now
+    customer_info: {
+        first_name: {
+            type: String
+        },
+        last_name: {
+            type: String
+        },
+        current_address: {
+            type: String,
+        },
+        email_address: {
+            type: String,
+        },
+        risk_money_laundering: {
+            type: [{
+                type: String,
+                enum: ['low', 'medium', 'high']
+            }],
+            default: ['medium']
+        },
+        last_modification_date: {
+            type: Date,
+            default: Date.now
+        },
     },
-  },
-  phone: {
-    type: String,
-  },
-  accounts: [{
-    iban: {
-      type: String
+    phone: {
+        type: String,
     },
-    total_amount: {
-      type: String
-    },
-    account_name: {
-      type: String
-    },
-    movements: [{
-      description: {
-        type: String
-      },
-      amount: {
-        type: String
-      },
-      movement_date: {
-        type: Date,
-        default: Date.now
-      }
-    }]
-  }],
-  advisor: { type: Schema.Types.ObjectId, ref: 'User' },
-  assigned_office: {
-    type: [{
-      type: String,
-      enum: [
-        'B. PRIVADA BARCELONA CENTRO',
-        'B. PRIVADA BARCELONA NORTE',
-        'B. PRIVADA MADRID CENTRO',
-        'B. PRIVADA MADRID NORTE',
-        'B. PRIVADA VALENCIA',
-        'B. PRIVADA ZARAGOZA',
-        'B. PUBLICA BARCELONA CENTRO',
-        'B. PUBLICA MADRID CENTRO',
-        'B. PUBLICA VALENCIA',
-        'B. PUBLICA ZARAGOZA'
-      ]
+    accounts: [{
+        iban: {
+            type: String
+        },
+        total_amount: {
+            type: String
+        },
+        account_name: {
+            type: String
+        },
+        movements: [{
+            description: {
+                type: String
+            },
+            amount: {
+                type: String
+            },
+            movement_date: {
+                type: Date,
+                default: Date.now
+            }
+        }]
     }],
-    default: ['B. PUBLICA MADRID CENTRO']
-  },
-  derivative_products: {
-    product1: { type: Boolean, default: false },
-    product2: { type: Boolean, default: false },
-    product3: { type: Boolean, default: false },
-    product4: { type: Boolean, default: false },
-    product5: { type: Boolean, default: false }
-  },
-  investment_products: [{name: String}]
+    advisor: {type: Schema.Types.ObjectId, ref: 'User'},
+    assigned_office: {
+        type: [{
+            type: String,
+            enum: [
+                'B. PRIVADA BARCELONA CENTRO',
+                'B. PRIVADA BARCELONA NORTE',
+                'B. PRIVADA MADRID CENTRO',
+                'B. PRIVADA MADRID NORTE',
+                'B. PRIVADA VALENCIA',
+                'B. PRIVADA ZARAGOZA',
+                'B. PUBLICA BARCELONA CENTRO',
+                'B. PUBLICA MADRID CENTRO',
+                'B. PUBLICA VALENCIA',
+                'B. PUBLICA ZARAGOZA'
+            ]
+        }],
+        default: ['B. PUBLICA MADRID CENTRO']
+    },
+    derivative_products: {
+        product1: {type: Boolean, default: false},
+        product2: {type: Boolean, default: false},
+        product3: {type: Boolean, default: false},
+        product4: {type: Boolean, default: false},
+        product5: {type: Boolean, default: false}
+    },
+    investment_products: {
+        familiar_group: {
+            tasks: {
+                type: String
+            },
+            campaigns: {
+                type: String
+            },
+            documents: {
+                type: String
+            }
+        },
+        economical_group: {
+            tasks: {
+                type: String
+            },
+            campaigns: {
+                type: String
+            },
+            documents: {
+                type: String
+            }
+        },
+    }
 });
 /*
 // Getter
