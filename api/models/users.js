692e054017829553f861992b9e9343c17cc0507b
lrcarrera <drewico@gmail.com>
2019-05-19 17:25:53 +0200

    Add role param to create new JWT token

--- api/models/users.js
+++ api/models/users.js
@@ -40,6 +40,7 @@ UserSchema.methods.generateJwt = function() {
     _id: this._id,
     email: this.email,
     name: this.name,
+    role: this.role,
     exp: parseInt(expiry.getTime() / 1000),
   }, "MY_SECRET"); // REMOVE FROM CODE IN FUTURE ITERATIONS
 };
